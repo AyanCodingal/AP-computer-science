51d57e7d66aaf2893f1540bd4f3ecf1659512e62
AyanCodingal <letsstudywithayan@gmail.com>
2025-03-05 22:44:31 +0530

    Add files via upload

---
+++ Java Basics/newuser.java
@@ -0,0 +1,16 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main{
+  public static void main(String[] args) {
+   for(int i=10; i>1; i--){
+    System.out.println("Thev vale of i is : " +i);
+   }
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
